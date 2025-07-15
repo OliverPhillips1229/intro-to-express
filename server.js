c5a458f32f8f8233786e98d9702803c6b32d4249
Oliver Levine <oliversfphillips@icloud.com>
2025-07-15 10:18:46 -0700

    addede comment to server.js and more codes with Ben

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+
+
 const express = require('express');
 const app = express();
 
+// hello world route
+// req = request, res = response
+// any changes made requires a refresh in the browser
+app.get('/', (req, res) => {
+  res.send('Hello World! I am here.');
+});
 
-
+// this is always LAST on the code
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
